10d4aab4b3ec3286a2a64f3daff19f3ca30e087c
awitherow <au.witherow@gmail.com>
2019-05-24 12:43:22 -1000

    add certs container

---
+++ js/index.js
@@ -0,0 +1,61 @@
+(function printCertifications() {
+  var certifications = [
+    {
+      logo: "rys_200.png",
+      title: "Registered Yoga School - 200",
+      description: "",
+      links: [
+        {
+          href:
+            "https://www.yogaalliance.org/Credentialing/Standards/200-HourStandards",
+          text: "Spirit of the Standards, RYS 200",
+        },
+      ],
+    },
+    {
+      logo: "rys_300.png",
+      title: "Registered Yoga School - 300",
+      description: "",
+      links: [
+        {
+          href:
+            "https://www.yogaalliance.org/Credentialing/Standards/300-HourStandards",
+          text: "Spirit of the Standards, RYS 300",
+        },
+      ],
+    },
+    {
+      logo: "gi_bill.png",
+      title: "GI Bill® Tuition and Housing Assistance",
+      description:
+        "The G.I. Bill can help eligible Active Duty Military or Active Duty Military Spouses cover costs with our Yoga Teacher Trainings!",
+      links: [
+        {
+          href: "https://benefits.va.gov/gibill/apply.asp",
+          text: "Check G.I. Bill Eligibility",
+        },
+        {
+          href: "./pages/contact.html",
+          text: "Get In Touch",
+        },
+      ],
+    },
+    {
+      logo: "my_caa.png",
+      title: "100% Tuition Assistance through MyCAA",
+      description:
+        "Are you the spouse of someone who is active in the Military? If so, you may be eligible for the Federal MyCAA Tuition Assistance Program.",
+      links: [
+        {
+          href:
+            "https://www.yogaalliance.org/Credentialing/Standards/300-HourStandards",
+          text: "Learn More",
+        },
+        {
+          href: "./pages/contact.html",
+          text: "Get In Touch",
+        },
+      ],
+    },
+  ];
+})();
