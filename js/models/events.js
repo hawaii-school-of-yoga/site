8441938d580d6a6fc7e24fcd53c887e154fef00a
Austin Witherow <au.witherow@gmail.com>
2023-09-12 10:48:23 -0400

    add upcoming events

--- js/models/events.js
+++ js/models/events.js
@@ -1,3 +1,49 @@
-function getRetreats() {
-  return [];
+function getEvents() {
+  return [
+    {
+      title: [
+        "CHANT IN ITALY WITH ZENY!",
+        "CHANTING FROM THE GUHA, THE CAVE OF THE HEART",
+      ],
+      link: "https://eurotas2023.com/tickets/",
+      image: "./static/img/events/eurotas2023.jpg",
+      dates: "October 4th - 8th, 2023",
+      description: [
+        "Chanting is food for the soul, when I chant my voice carries me somewhere.  But that somewhere is inside the cave of my heart.  In Sanskrit we call this heart cave- Guha.",
+        "In this presentation Zeny shares the practice of Sacred Chanting and how ancient mantras and chants connect us to the wisdom of our ancestors.   By cultivating a daily practice of meditating with mantras and using the human voice, we can connect to what the Nada Yogis of India call the ‘Guha’ the heart cave, also known as the Anahata Chakra.  This heart space contains tremendous power for transformation and self-healing, for ourselves and for the world.",
+      ],
+    },
+    {
+      title: [
+        "SONIC THEOLOGY",
+        "The Metaphysical Understanding of Sound Therapy, and the Attainment of Spiritual Harmonic Knowledge / First Module of the Sound Therapy Certification of 9Ways Academia",
+      ],
+      link: "https://9ways.ticketleap.com/metaphysics-2024/details",
+      image: "./static/img/events/sonic-theology.jpeg",
+      dates: "February 24 to March 4, 2024",
+      description: [
+        "Sonic Theology delves deep into Western, Mid Eastern, and Far Eastern sound therapy theories. With a sweeping 170,000-year analysis, it bridges the gaps left by Western sound healing programs.",
+        "This unique course re-examines sound history from a matrifocal lens, shedding light on the restrictions of a patriarchal history model and illuminating the profound roles of Sound, Trance, and Dance in our cultural foundation.",
+        "Discover the age-old systems influencing the Western model, from Pythagoras' learnings in India to the influence of the House of Wisdom. As Europe faced the Dark Ages, Pythagorean philosophy thrived in Central Asia, with secret societies championing sound's healing power.",
+        "This intensive course, refined over five decades of rigorous research, offers insights from a faculty boasting over 200 years of collective expertise. Most sound healing courses today merely teach instruments, but *Sonic Theology* delves into the when, why, and what of sound.",
+        "Join our esteemed faculty including Dr. Mitch Nur, Mallika Paritosh, and others. Enrollment is exclusive to 11 students annually, ensuring in-depth instruction and hands-on experience.",
+      ],
+    },
+    {
+      title: ["Bonpo Shang Transmission"],
+      link: "https://9ways.ticketleap.com/metaphysics-2024/details",
+      image: "./static/img/events/shang.jpeg",
+      dates: "Europe 2024, USA 2025",
+      description: [
+        "Offered in Europe in 2024 and USA in 2025 with Mitch Nur, PhD, Umzey Zeny Bagatsing Ogrisseg, and Nancy Aleo.",
+        "The Bonpo Shang, dating back to ancient ZhangZhung, is among the world's oldest metal sound tools. It's deeply rooted in the Yungdrung Bon tradition, attributed to the founder, Tonpa Shenrab, and possibly predating 16,000BC.",
+        "Its significance? Dr. Mitch believes the Shang might be the only genuine sound healing instrument. Authentic texts detail its creation, use, and wielders - primarily 'knowledge holders'. Most Shangs today aren't genuine.",
+        "Mitch, alongside Umzey Zeny and Nancy, offers an immersive experience. The course takes participants back to a time preceding Tibet, Buddhism, and the Yungdrung Bon, to the age of the Shen.",
+        "Innerview: Expect intensive training. Starting 4:30am, the program is rooted in decades of research, interactions with Lamas, and Dr. Mitch's comprehensive lineage in Bon practices.",
+        "Outerview: This course is the most thorough on the Shang globally, delving deep into the Bonpo culture and further into its shamanic uses.",
+        "The journey encompasses pre-Tibetan history, Drala and Werma beliefs, and practices from the Shen period. The core is the Northern Treasury texts, particularly the Po Ti.",
+        "Mitch Nur, PhD emphasizes the historical depth, highlighting the Shang's profound role in ancient healing practices, its misrepresentation in modern times, and the authentic practices he introduces based on ancient texts and mantras.",
+      ],
+    },
+  ];
 }
--- js/views/events.js
+++ js/views/events.js
@@ -1,11 +1,72 @@
+function Event(event, id) {
+  return `
+    <div class="card mb-4 shadow-sm">
+      <img src="${event.image}" alt="${event.title[0]}" class="card-img-top">
+      <div class="card-body">
+        <h4 class="card-title"><a href="${event.link}" class="text-primary">${
+    event.title[0]
+  }</a></h4>
+        ${
+          event.title[1]
+            ? `<h5 class="card-subtitle mb-2 text-muted">${event.title[1]}</h5>`
+            : ""
+        }
+        ${event.dates ? `<p class="text-info">${event.dates}</p>` : ""}
+        <p class="card-text">${event.description[0]}</p>
+        <div class="d-flex">
+            <a href="${
+              event.link
+            }" class="btn btn-primary btn-sm mr-2">Sign Up</a>
+            <button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#eventModal${id}">Learn More</button>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function EventModal(event, id) {
+  return `
+    <div style="z-index:99999" class="modal fade" id="eventModal${id}" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel${id}" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="eventModalLabel${id}">${
+    event.title[0]
+  }</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img src="${event.image}" alt="${
+    event.title[0]
+  }" class="img-fluid mb-3">
+            ${event.title[1] ? `<h6>${event.title[1]}</h6>` : ""}
+            ${event.dates ? `<p class="text-info">${event.dates}</p>` : ""}
+            <p>${event.description.join("<br/><br/>")}</p>
+          </div>
+          <div class="modal-footer">
+            <a href="${event.link}" class="btn btn-primary">Sign Up</a>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
 (function () {
   var events = getEvents();
-  var container = document.getElementById("events-container");
-  var HTML = "";
+  var container = document.getElementById(`events-container`);
 
-  for (i = 0; i <= events.length - 1; i++) {
-    HTML += Retreat(events[i]);
-  }
+  events.map((event, index) => {
+    var HTML = `
+      <div class="col-lg-4 col-md-6 mb-4">
+          ${Event(event, index)}
+      </div>
+      ${EventModal(event, index)}
+    `;
 
-  container.innerHTML = HTML;
+    container.innerHTML += HTML;
+  });
 })();
